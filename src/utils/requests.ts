92e31be07cbf40041bce3f033ef06932c5a3c1d4
xXQiuChenXx <lautaihong2004@gmail.com>
2024-07-06 22:25:30 +0800

    Create requests.ts

---
+++ src/utils/requests.ts
@@ -0,0 +1,12 @@
+import fetch from "node-fetch";
+
+async function adminRequest(method: string, endpoint: string, data?: any) {
+
+}
+
+
+async function userRequest() {
+
+}
+
+export { adminRequest, userRequest };
