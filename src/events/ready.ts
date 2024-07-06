f3bc59aa6148c074df93f7f514ed04005f784fd3
xXQiuChenXx <lautaihong2004@gmail.com>
2024-07-06 23:23:51 +0800

    updates

--- src/events/ready.ts
+++ src/events/ready.ts
@@ -1,9 +1,18 @@
-import { Client, PresenceStatusData } from "discord.js";
+import { ActivityType, Client, PresenceStatusData } from "discord.js";
 import config from "@/config.json";
 import Pterodragon from "../Pterodragon";
 
 export default async function onReady(bot: Client, client: Pterodragon) {
     console.log(`Logged in as ${bot.user?.username}`);
+
+    await bot.user?.setPresence({ 
+        afk: config.botActivity.afk,
+        status: config.botActivity.status as PresenceStatusData,
+        activities: [{
+            name: config.botActivity.name,
+            type: ActivityType.Playing,
+        }]
+    })
 
     process.stdin.on("data", (data) => {
         const cmd = (data.toString().trim()).split(" ")[0];
