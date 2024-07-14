2a665831ff166e2ed52669b59bfd56b77744c781
xXQiuChenXx <lautaihong2004@gmail.com>
2024-07-14 21:58:52 +0800

    database

---
+++ src/drizzle.config.ts
@@ -0,0 +1,11 @@
+import { defineConfig } from "drizzle-kit";
+import config from "@/config.json";
+
+export default defineConfig({
+  schema: "./src/drizzle/schema.ts",
+  out: "./src/drizzle/migrations",
+  dialect: "mysql",
+  dbCredentials: config.mysql,
+  verbose: true,
+  strict: true
+});
