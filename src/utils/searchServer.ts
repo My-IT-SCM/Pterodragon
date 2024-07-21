3aa71e3e924d4cea6c8ef99e3ee39659aafa2c23
xXQiuChenXx <lautaihong2004@gmail.com>
2024-07-21 22:19:50 +0800

    Create searchServer.ts

---
+++ src/utils/searchServer.ts
@@ -0,0 +1,8 @@
+interface Props {
+  server: String;
+  type: "searchById" | "searchByName" | "searchByUUID" | "searchByEID";
+}
+
+export const searchServer = async ({ server, type }: Props) => {
+    
+};
